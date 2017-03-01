fc2a0c9a9e276986d0440bbdce7883fb7b61c42b
Kayla <kayla.comalli@gmail.com>
2017-03-01 15:42:57 -0500

    trying to get functionality in admin view

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,8 @@
 RailsAdmin.config do |config|
 
+  config.assets.initialize_on_precompile = false
+  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+
   ### Popular gems integration
 
   ## == Devise ==
