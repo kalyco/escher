576e99ab31999c0c9e235cd65bed14326d2ff499
Kayla <kayla.comalli@gmail.com>
2017-04-22 20:37:57 -0400

    writing techniques page. getting some weirdness

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -18,6 +18,30 @@ class PiecesController < ApplicationController
 
   end
 
+  def lithographs_and_mezzotints
+    if params[:search]
+      @pieces = Piece.search(params[:search]).order("featured DESC")
+    else
+      @pieces = Piece.all.where(category: ("mezzotint" || "lithograph")).order('featured DESC')
+    end
+  end
+
+  def woodcuts
+    if params[:search]
+      @pieces = Piece.search(params[:search]).order("featured DESC")
+    else
+      @pieces = Piece.all.where(category: ("woodcut")).order('featured DESC')
+    end
+  end
+
+  def watercolors
+    if params[:search]
+      @pieces = Piece.search(params[:search]).order("featured DESC")
+    else
+      @pieces = Piece.all.where(category: ("watercolors")).order('featured DESC')
+    end
+  end
+
   private
   	def set_pieces
 		 @pieces = Piece.all
@@ -26,7 +50,6 @@ class PiecesController < ApplicationController
     def set_piece
      # @piece = Piece.find(params[:id])
     end
-
 
     def for_sale
       @pieces = Pieces.where(for_sale: true)
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
 	  resources :pieces
 	  get "pieces/original_works" => "pieces/original_works", as: "originals"
 
-	  get "about" => "static_pages/about", as: "about"
-	  get "about" => "static_pages/techniques", as: "techniques"
+	  get "about" => "static_pages#about", as: "about"
+	  get "techniques" => "static_pages#techniques", as: "techniques"
+	  get "mezzotint" => "static_pages#mezzotint", as: "mezzotint"
+	  get "original_print" => "static_pages#original_print", as: "original_print"
+	  get "fakes_and_mistakes" => "static_pages#fakes_and_mistakes", as: "fakes_and_mistakes"
 
 	  resources :tags
 end
