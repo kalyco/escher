86f6e4c42725142af42524140aeec3b55922fda7
Kayla <kayla.comalli@gmail.com>
2017-05-05 13:04:51 -0400

    new head

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -33,3 +33,13 @@ $(".close-search").click(function(){
   $(".search-nav").removeClass("is-active", 350);
   $(".main-nav").removeClass("is-hidden", 350);
 });
+
+$(document).ready(function () {
+    $('img').on('click', function () {
+        var image = $(this).attr('src');
+        //alert(image);
+        $('#myModal').on('show.bs.modal', function () {
+            $(".showimage").attr("src", image);
+        });
+    });
+});
